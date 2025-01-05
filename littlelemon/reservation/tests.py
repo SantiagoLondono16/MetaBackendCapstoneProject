2af7483159704d33a74d6909b7385a2b39d09da2
Santiago Londoño Giraldo <slondonogi@unal.edu.co>
2025-01-04 23:52:23 -0500

    Tests

--- littlelemon/reservation/tests.py
+++ littlelemon/reservation/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Menu
 
-# Create your tests here.
+class MenuTest(TestCase):
+    def test_instance(self):
+        menu = Menu.objects.create(Title='Ice cream', Price=80, Inventory = 100)
+        result = f'{menu.Title} : {menu.Price}'
+        self.assertEqual(result, 'Ice cream : 80')
